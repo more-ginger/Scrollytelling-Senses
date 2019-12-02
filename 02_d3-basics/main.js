ca991af293f4c2d73fae23c74cf8d527c0e14aec
more-ginger <francesca1morini@gmail.com>
2019-12-02 16:28:28 +0100

    add scale example

--- 02_d3-basics/main.js
+++ 02_d3-basics/main.js
@@ -1,5 +1,5 @@
 //Takes a selector and return a selection representing all the elements that match the selector, here you are selecting the svg circles in the HTML file!
-var circle = d3.selectAll("circle");
+/var circle = d3.selectAll("circle");
 
 //2.Selecting Elements means you can also edit them
 circle.style("fill","steelblue");
@@ -11,10 +11,9 @@ circle.attr("cx",function() {return Math.random() * 720;});
 
 //Time to bring data in!
 //Let's create some variables for data (We will use them later)
-var dataOne = [32,57,112];
-var dataTwo = [32,57,112,293];
-var dataThree = [32,57];
-
+var dataOne = [32,57,112]
+var dataTwo = [32,57,112,293]
+var dataThree = [32,57]
 
 //2B.Binding data
 //Data is specified as an array of values, one element of the array is called "Datum"
@@ -33,7 +32,7 @@ circle.attr("cx", function(d,i) {return i * 100 + 30;});
 var svg = d3.select("svg");
 
 var circle = svg.selectAll("circle")
-      .data(dataTwo);
+       .data(dataTwo);
 
 //Adding one element by entering the selection computed by a data join. We are creating a circle for
 //the missing data, without having to draw it manually in the svg
@@ -44,32 +43,39 @@ circleEnter.attr("cy", 60);
 circleEnter.attr("cx", function(d,i) {return i * 100 + 30;});
 circleEnter.attr("r", function(d) {return Math.sqrt(d);});
 
-// //The structure of this data join is very common: selectAll + data + enter + append
-// svg.selectAll("circle")
-//     .data(dataTwo)
-//   .enter().append("circle")
-//     .attr("cx", function(d,i) {return i * 100 + 30;})
-//     .attr("r", function(d) {return Math.sqrt(d);});
-
+//The structure of this data join is very common: selectAll + data + enter + append
+svg.selectAll("circle")
+  data(dataTwo)
+.enter().append("circle")
+ .attr("cx", function(d,i) {return i * 100 + 30;})
+ .attr("r", function(d) {return Math.sqrt(d);});
 
 //4.Exiting Elements
 //To remove exceeding elements you can use exit(), this is the mirror of enter. By binding the last (and shorter) dataset we can remove circles.
 var circle = svg.selectAll("circle")
-        .data(dataThree);
-
+     .data(dataThree);
 circle.exit().remove();
 
-//5. All together!
+//5. Scale!
+var width = 500
+var x = d3.scaleLinear()
+    .domain([0, d3.max(dataTwo)])
+    .range([0, width]);
+
+
+////////////////////////////////////////////////
+//6. All together!
 //SelectAll return an empty selection
+
+var svg = d3.select("svg")
 var circle = svg.selectAll("circle")
-//This selection is joined by an array of data, by writing console.log you will notice that selection.data returns _enter and _exit, while _enter has pending elements
-//_enter is currently empty because there are no elements that need to be removed.
-      .data(dataOne, function(d) {return d ;});
-      console.log(circle);
+  //This selection is joined by an array of data, by writing console.log you will notice that selection.data returns _enter and _exit, while _enter has pending elements
+  //_enter is currently empty because there are no elements that need to be removed.
+        .data(dataTwo, function(d) {return d ;});
 
-circle.enter().append("circle")
-    .attr("cy",60)
-    .attr("cx", function(d,i) {return i * 100 + 30;})
-    .attr("r", function(d) {return Math.sqrt(d);});
+    circle.enter().append("circle")
+        .attr("cy",60)
+        .attr("cx", function(d,i) {return i * 100 + 30;})
+        .attr("r", function(d) {return Math.sqrt(d);});
 
-circle.exit().remove();
+    circle.exit().remove();
