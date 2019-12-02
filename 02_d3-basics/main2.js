8307b9e19ecc94a12a7b6493d96ebaa851c84afc
more-ginger <francesca1morini@gmail.com>
2019-12-02 13:04:54 +0100

    Update JS for basic D3

--- 02_d3-basics/main2.js
+++ 02_d3-basics/main2.js
@@ -1,20 +1,14 @@
-// 433708
-var dataOne = [50, 50, 50]
-var dataTwo = [54, 76, 49, 55, 90, 91]
-var dataThree = [5, 10, 11]
+var circle = d3.selectAll("circle")
 
+circle.style("fill", "steelblue")
+circle.style("stroke", "red")
+circle.attr("r", 30)
 
+//circle.attr("cx", d => {return Math.random() * 700})
 
-var svg = d3.select("svg")
-var circle = svg.selectAll("circle")
-      .data(dataTwo)
+var dataOne = [33, 57, 112]
 
-var circleEnter = circle.enter().append("circle")
-circleEnter.attr("cy", 60)
-circleEnter.attr("cx", function(d,i) {return i * 100 + 30;});
-circleEnter.attr("r", function(d) {return Math.sqrt(d);});
+circle.data(dataOne)
+circle.attr("r", d => {return Math.sqrt(d)})
 
-var circle = svg.selectAll("circle")
-  .data(dataThree)
-
-circle.exit().remove()
+circle.attr("cx", function(d,i) {return i * 100 + 30})
